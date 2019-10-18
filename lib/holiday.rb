cbf2737fdb702fa42fe5f03a114c803d18009a99
Andrew Capp <acapp909@gmail.com>
2019-10-18 03:56:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,13 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].each do |holiday, supplies|
+    supplies_array << supplies
+
+
+  end
+    supplies_array
+
 
 end
 

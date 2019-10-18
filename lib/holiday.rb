e49e65361abb00bdd7f92ca5d8cce35eb23fcc9d
Andrew Capp <acapp909@gmail.com>
2019-10-18 04:46:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,10 +85,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_info|
     puts "#{season}:".capitalize
     holiday_info.each do |holiday, supplies|
-      holiday_puts += "#{holiday}".split("_").map{|item| item.capitalize}.
+      holiday_puts = "#{holiday}".split("_").map{|item| item.capitalize}.
         join(" ")
         #holiday_puts = holiday_puts.join(" ")
-        holiday_puts = "  #{holiday_puts}: "
+        holiday_puts += "  #{holiday_puts}: "
         supplies.each_with_index do |supply, index|
           if index == 0
             holiday_puts = holiday_puts + "#{supply}"
